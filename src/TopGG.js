a6385b06c9cdb8443a91ff0166cae1d6c9c55527
MrBaskan <117309746+MrBaskan33@users.noreply.github.com>
2025-05-04 10:24:21 +0300

    Create TopGG.js

---
+++ src/TopGG.js
@@ -0,0 +1,32 @@
+const { initCheckUpdates } = require("../checkUpdate.js")
+const axios = require("axios")
+
+class TopGGChecker {
+  constructor(topggToken, botId) {
+    this.token = topggToken
+    this.botId = botId
+    this.baseURL = "https://top.gg/api"
+    this.checkUpdate = true
+  }
+
+  async isVoted(userId) {
+    try {
+      const response = await axios.get(
+        `/bots/${this.botId}/check?userId=${userId}`,
+        {
+          baseURL: this.baseURL,
+          headers: {
+            Authorization: this.token,
+          },
+        }
+      )
+      if(response.status === 200) {
+        return response.data.voted === 1
+      }
+    } catch (error) {
+      throw new Error("Top.gg oy bilgisi kontrol edilirken hata oluştu.")
+    }
+  }
+}
+
+module.exports = { TopGGChecker }
